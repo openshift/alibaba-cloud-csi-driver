89e4837e9405663d509963d92ed1a2eb39a6f2ed
Jan Safranek <jsafrane@redhat.com>
2022-01-19 15:53:37 +0100

    UPSTREAM: 572: Adapt to openshift, read ak from /root/.alibabacloud/credentials path
    This is heavily tailored patch from upstream, I basically took
    pkg/utils/auth.go from master.

--- pkg/utils/auth.go
+++ pkg/utils/auth.go
@@ -17,16 +17,31 @@ limitations under the License.
 package utils
 
 import (
+	"bytes"
 	"crypto/aes"
 	"crypto/cipher"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/tls"
+	"crypto/x509"
+	"crypto/x509/pkix"
 	"encoding/base64"
 	"encoding/json"
+	"encoding/pem"
+	"errors"
+	"fmt"
 	log "github.com/sirupsen/logrus"
+	"google.golang.org/grpc/credentials"
 	"io/ioutil"
+	"math/big"
 	"os"
 	"path/filepath"
 	"strings"
 	"time"
+
+	"github.com/aliyun/alibaba-cloud-sdk-go/sdk"
+	"github.com/aliyun/alibaba-cloud-sdk-go/sdk/auth"
+	"github.com/aliyun/alibaba-cloud-sdk-go/sdk/auth/credentials/provider"
 )
 
 const (
@@ -46,96 +61,169 @@ type AKInfo struct {
 	Expiration string `json:"expiration"`
 	// Keyring key ring
 	Keyring string `json:"keyring"`
+	// RoleAccessKeyId key
+	RoleAccessKeyID string `json:"role.access.key.id"`
+	// RoleAccessKeySecret key
+	RoleAccessKeySecret string `json:"role.access.key.secret"`
+	// RoleArn key
+	RoleArn string `json:"role.arn"`
 }
 
-// GetDefaultAK read default ak from local file or from STS
-func GetDefaultAK() (string, string, string) {
-	accessKeyID, accessSecret := GetLocalAK()
+// ManageTokens 定义资源账号 和 角色扮演账号
+type ManageTokens struct {
+	// AccessKeyId key
+	AccessKeyID string
+	// AccessKeySecret key
+	AccessKeySecret string
+	// SecurityToken key
+	SecurityToken string
 
-	accessToken := ""
-	if accessKeyID == "" || accessSecret == "" {
-		accessKeyID, accessSecret, accessToken = GetManagedToken()
-		if accessKeyID != "" {
-			log.Infof("Get AK: use Managed AK")
-		}
-	} else {
-		log.Infof("Get AK: use Local AK")
+	// RoleAccessKeyId key
+	RoleAccessKeyID string
+	// RoleAccessKeySecret key
+	RoleAccessKeySecret string
+	// RoleArn key
+	RoleArn string
+}
+
+// KeyPairArtifacts is cert struct
+type KeyPairArtifacts struct {
+	Cert    *x509.Certificate
+	Key     *rsa.PrivateKey
+	CertPEM []byte
+	KeyPEM  []byte
+}
+
+// CertOption is cert option
+type CertOption struct {
+	CAName          string
+	CAOrganizations []string
+	DNSNames        []string
+	CommonName      string
+}
+
+// AccessControlMode is int, represents different modes
+type AccessControlMode int
+
+// AccessControlMode includes AccessKey, ManagedToken, EcsRamRole, Credential, RoleArnToken, five types of access control
+const (
+	AccessKey AccessControlMode = iota
+	ManagedToken
+	EcsRAMRole
+	Credential
+	RoleArnToken
+)
+
+// AccessControl is access control option
+type AccessControl struct {
+	AccessKeyID     string
+	AccessKeySecret string
+	StsToken        string
+	RoleArn         string
+	Config          *sdk.Config
+	Credential      auth.Credential
+	UseMode         AccessControlMode
+}
+
+func getAddonToken() AccessControl {
+	tokens := getManagedToken()
+	return AccessControl{AccessKeyID: tokens.AccessKeyID, AccessKeySecret: tokens.AccessKeySecret, StsToken: tokens.SecurityToken, UseMode: ManagedToken}
+}
+
+// GetAccessControl  1、Read default ak from local file. 2、If local default ak is not exist, then read from STS.
+func GetAccessControl() AccessControl {
+	//1、Get AK from Env
+	acLocalAK := GetLocalAK()
+	if len(acLocalAK.AccessKeyID) != 0 && len(acLocalAK.AccessKeySecret) != 0 {
+		log.Info("Get AK: use Local AK")
+		return acLocalAK
 	}
 
-	if accessKeyID == "" || accessSecret == "" {
-		accessKeyID, accessSecret, accessToken = GetSTSAK()
-		log.Infof("Get AK: use STS")
+	//2、Get AK from Credential Files
+	acCredentialAK := getCredentialAK()
+	if acCredentialAK.Config != nil && acCredentialAK.Credential != nil {
+		log.Info("Get AK: use Credential AK")
+		return acCredentialAK
 	}
 
-	return accessKeyID, accessSecret, accessToken
+	//3、Get AK from ManagedToken
+	acAddonToken := getAddonToken()
+	if len(acAddonToken.AccessKeyID) != 0 {
+		log.Info("Get AK: use Addon Token")
+		return acAddonToken
+	}
+
+	//4、Get AK from StsToken
+	acStsToken := getStsToken()
+	log.Info("Get AK: use Sts Token")
+	return acStsToken
 }
 
 // GetLocalAK read ossfs ak from local or from secret file
-func GetLocalAK() (string, string) {
+func GetLocalAK() AccessControl {
 	accessKeyID, accessSecret := "", ""
 	accessKeyID = os.Getenv("ACCESS_KEY_ID")
 	accessSecret = os.Getenv("ACCESS_KEY_SECRET")
 
-	return strings.TrimSpace(accessKeyID), strings.TrimSpace(accessSecret)
+	return AccessControl{AccessKeyID: strings.TrimSpace(accessKeyID), AccessKeySecret: strings.TrimSpace(accessSecret), UseMode: AccessKey}
 }
 
-// GetSTSAK get STS AK and token from ecs meta server
-func GetSTSAK() (string, string, string) {
+// GetStsToken get STS token and token from ecs meta server
+func getStsToken() AccessControl {
 	roleAuth := RoleAuth{}
 	subpath := "ram/security-credentials/"
 	roleName, err := GetMetaData(subpath)
 	if err != nil {
 		log.Errorf("GetSTSToken: request roleName with error: %s", err.Error())
-		return "", "", ""
+		return AccessControl{}
 	}
 
 	fullPath := filepath.Join(subpath, roleName)
 	roleInfo, err := GetMetaData(fullPath)
 	if err != nil {
 		log.Errorf("GetSTSToken: request roleInfo with error: %s", err.Error())
-		return "", "", ""
+		return AccessControl{}
 	}
 
 	err = json.Unmarshal([]byte(roleInfo), &roleAuth)
 	if err != nil {
 		log.Errorf("GetSTSToken: unmarshal roleInfo: %s, with error: %s", roleInfo, err.Error())
-		return "", "", ""
+		return AccessControl{}
 	}
-	return roleAuth.AccessKeyID, roleAuth.AccessKeySecret, roleAuth.SecurityToken
+	return AccessControl{AccessKeyID: roleAuth.AccessKeyID, AccessKeySecret: roleAuth.AccessKeySecret, StsToken: roleAuth.SecurityToken, UseMode: EcsRAMRole}
 }
 
 // GetManagedToken get ak from csi secret
-func GetManagedToken() (string, string, string) {
+func getManagedToken() (tokens ManageTokens) {
 	var akInfo AKInfo
-	AccessKeyID, AccessKeySecret, SecurityToken := "", "", ""
 	if _, err := os.Stat(ConfigPath); err == nil {
 		encodeTokenCfg, err := ioutil.ReadFile(ConfigPath)
 		if err != nil {
 			log.Errorf("failed to read token config, err: %v", err)
-			return "", "", ""
+			return ManageTokens{}
 		}
 		err = json.Unmarshal(encodeTokenCfg, &akInfo)
 		if err != nil {
 			log.Errorf("error unmarshal token config: %v", err)
-			return "", "", ""
+			return ManageTokens{}
 		}
 		keyring := akInfo.Keyring
 		ak, err := Decrypt(akInfo.AccessKeyID, []byte(keyring))
 		if err != nil {
 			log.Errorf("failed to decode ak, err: %v", err)
-			return "", "", ""
+			return ManageTokens{}
 		}
 
 		sk, err := Decrypt(akInfo.AccessKeySecret, []byte(keyring))
 		if err != nil {
 			log.Errorf("failed to decode sk, err: %v", err)
-			return "", "", ""
+			return ManageTokens{}
 		}
 
 		token, err := Decrypt(akInfo.SecurityToken, []byte(keyring))
 		if err != nil {
 			log.Errorf("failed to decode token, err: %v", err)
-			return "", "", ""
+			return ManageTokens{}
 		}
 		layout := "2006-01-02T15:04:05Z"
 		t, err := time.Parse(layout, akInfo.Expiration)
@@ -145,11 +233,27 @@ func GetManagedToken() (string, string, string) {
 		if t.Before(time.Now()) {
 			log.Errorf("invalid token which is expired, expiration as: %s", akInfo.Expiration)
 		}
-		AccessKeyID = string(ak)
-		AccessKeySecret = string(sk)
-		SecurityToken = string(token)
+		tokens.AccessKeyID = string(ak)
+		tokens.AccessKeySecret = string(sk)
+		tokens.SecurityToken = string(token)
+
+		if akInfo.RoleAccessKeyID != "" && akInfo.RoleAccessKeySecret != "" {
+			roleAK, err := Decrypt(akInfo.RoleAccessKeyID, []byte(keyring))
+			if err != nil {
+				log.Errorf("failed to decode role ak, err: %v", err)
+				return ManageTokens{}
+			}
+			roleSK, err := Decrypt(akInfo.RoleAccessKeySecret, []byte(keyring))
+			if err != nil {
+				log.Errorf("failed to decode role sk, err : %v", err)
+				return ManageTokens{}
+			}
+			tokens.RoleAccessKeyID = string(roleAK)
+			tokens.RoleAccessKeySecret = string(roleSK)
+		}
+		tokens.RoleArn = akInfo.RoleArn
 	}
-	return AccessKeyID, AccessKeySecret, SecurityToken
+	return tokens
 }
 
 // PKCS5UnPadding get pkc
@@ -181,4 +285,180 @@ func Decrypt(s string, keyring []byte) ([]byte, error) {
 
 	origData = PKCS5UnPadding(origData)
 	return origData, nil
+}
+
+// GetDefaultRoleAK  返回角色扮演账号AK, SK, role arn
+func GetDefaultRoleAK() AccessControl {
+	roleAccessKeyID, roleAccessKeySecret, roleArn := os.Getenv("ROLE_ACCESS_KEY_ID"), os.Getenv("ROLE_ACCESS_KEY_SECRET"), os.Getenv("ROLE_ARN")
+	if len(roleAccessKeyID) == 0 || len(roleAccessKeySecret) == 0 || len(roleArn) == 0 {
+		tokens := getManagedToken()
+		return AccessControl{AccessKeyID: tokens.RoleAccessKeyID, AccessKeySecret: tokens.RoleAccessKeySecret, RoleArn: tokens.RoleArn, UseMode: ManagedToken}
+	}
+	return AccessControl{AccessKeyID: roleAccessKeyID, AccessKeySecret: roleAccessKeySecret, RoleArn: roleArn, UseMode: RoleArnToken}
+}
+
+// CreateCACert function is create cacert
+func CreateCACert(option CertOption, begin, end time.Time) (*KeyPairArtifacts, error) {
+	templ := &x509.Certificate{
+		SerialNumber: big.NewInt(0),
+		Subject: pkix.Name{
+			CommonName:   option.CAName,
+			Organization: option.CAOrganizations,
+		},
+		DNSNames:              option.DNSNames,
+		NotBefore:             begin,
+		NotAfter:              end,
+		KeyUsage:              x509.KeyUsageDigitalSignature | x509.KeyUsageKeyEncipherment | x509.KeyUsageCertSign,
+		BasicConstraintsValid: true,
+		IsCA:                  true,
+	}
+	key, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		return nil, fmt.Errorf("generating key: %s", err)
+	}
+	der, err := x509.CreateCertificate(rand.Reader, templ, templ, key.Public(), key)
+	if err != nil {
+		return nil, fmt.Errorf("creating certificate: %s", err)
+	}
+	certPEM, keyPEM, err := pemEncode(der, key)
+	if err != nil {
+		return nil, fmt.Errorf("encoding PEM: %s", err)
+	}
+	cert, err := x509.ParseCertificate(der)
+	if err != nil {
+		return nil, fmt.Errorf("parsing certificate: %s", err)
+	}
+
+	return &KeyPairArtifacts{Cert: cert, Key: key, CertPEM: certPEM, KeyPEM: keyPEM}, nil
+}
+
+// CreateCertPEM function is create cacert pem
+func CreateCertPEM(option CertOption, ca *KeyPairArtifacts, begin, end time.Time, isClient bool) ([]byte, []byte, error) {
+	sn, err := genSerialNum()
+	if err != nil {
+		return nil, nil, err
+	}
+	eks := []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth}
+	dnsNames := option.DNSNames
+	if isClient {
+		eks = []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth}
+		dnsNames = nil
+	}
+	templ := &x509.Certificate{
+		SerialNumber: sn,
+		Subject: pkix.Name{
+			CommonName: option.CommonName,
+		},
+		DNSNames:              dnsNames,
+		NotBefore:             begin,
+		NotAfter:              end,
+		KeyUsage:              x509.KeyUsageDigitalSignature | x509.KeyUsageKeyEncipherment,
+		ExtKeyUsage:           eks,
+		BasicConstraintsValid: true,
+	}
+	key, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		return nil, nil, fmt.Errorf("generating key: %s", err)
+	}
+	der, err := x509.CreateCertificate(rand.Reader, templ, ca.Cert, key.Public(), ca.Key)
+	if err != nil {
+		return nil, nil, fmt.Errorf("creating certificate: %s", err)
+	}
+	certPEM, keyPEM, err := pemEncode(der, key)
+	if err != nil {
+		return nil, nil, fmt.Errorf("encoding PEM: %s", err)
+	}
+	return certPEM, keyPEM, nil
+}
+
+func pemEncode(certificateDER []byte, key *rsa.PrivateKey) ([]byte, []byte, error) {
+	certBuf := &bytes.Buffer{}
+	if err := pem.Encode(certBuf, &pem.Block{Type: "CERTIFICATE", Bytes: certificateDER}); err != nil {
+		return nil, nil, fmt.Errorf("encoding cert: %s", err)
+	}
+	keyBuf := &bytes.Buffer{}
+	if err := pem.Encode(keyBuf, &pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(key)}); err != nil {
+		return nil, nil, fmt.Errorf("encoding key: %s", err)
+	}
+	return certBuf.Bytes(), keyBuf.Bytes(), nil
+}
+
+func genSerialNum() (*big.Int, error) {
+	serialNumLimit := new(big.Int).Lsh(big.NewInt(1), 128)
+	serialNum, err := rand.Int(rand.Reader, serialNumLimit)
+	if err != nil {
+		return nil, fmt.Errorf("serial number generation failure (%v)", err)
+	}
+	return serialNum, nil
+}
+
+// NewClientTLSFromFile function is new client with tls
+func NewClientTLSFromFile(serverName, caFile, certFile, keyFile string) (credentials.TransportCredentials, error) {
+	// Load the certificates from disk
+	certificate, err := tls.LoadX509KeyPair(certFile, keyFile)
+	if err != nil {
+		return nil, fmt.Errorf("could not load server key pair: %s", err)
+	}
+
+	// Create a certificate pool from the certificate authority
+	certPool := x509.NewCertPool()
+	ca, err := ioutil.ReadFile(caFile)
+	if err != nil {
+		return nil, fmt.Errorf("could not read ca certificate: %s", err)
+	}
+
+	// Append the client certificates from the CA
+	if ok := certPool.AppendCertsFromPEM(ca); !ok {
+		return nil, errors.New("failed to append client certs")
+	}
+
+	// Create the TLS credentials
+	creds := credentials.NewTLS(&tls.Config{
+		ServerName:   serverName,
+		Certificates: []tls.Certificate{certificate},
+		RootCAs:      certPool,
+	})
+	return creds, nil
+}
+
+// NewServerTLSFromFile function is new server with tls
+func NewServerTLSFromFile(caFile, certFile, keyFile string) (credentials.TransportCredentials, error) {
+	// Load the certificates from disk
+	certificate, err := tls.LoadX509KeyPair(certFile, keyFile)
+	if err != nil {
+		return nil, fmt.Errorf("could not load server key pair: %s", err)
+	}
+
+	// Create a certificate pool from the certificate authority
+	certPool := x509.NewCertPool()
+	ca, err := ioutil.ReadFile(caFile)
+	if err != nil {
+		return nil, fmt.Errorf("could not read ca certificate: %s", err)
+	}
+
+	// Append the client certificates from the CA
+	if ok := certPool.AppendCertsFromPEM(ca); !ok {
+		return nil, errors.New("failed to append client certs")
+	}
+
+	// Create the TLS credentials
+	creds := credentials.NewTLS(&tls.Config{
+		ClientAuth:   tls.RequireAndVerifyClientCert,
+		Certificates: []tls.Certificate{certificate},
+		ClientCAs:    certPool,
+	})
+	return creds, nil
+}
+
+// getCredentialAK get credential and config from credential files.
+func getCredentialAK() AccessControl {
+	envProvider := provider.NewEnvProvider()
+	profileProvider := provider.NewProfileProvider()
+	pc := provider.NewProviderChain([]provider.Provider{envProvider, profileProvider})
+	credential, err := pc.Resolve()
+	if err != nil {
+		log.Errorf("Failed to resolve an authentication provider: %v", err)
+	}
+	config := sdk.NewConfig().WithScheme("https")
+	return AccessControl{Config: config, Credential: credential, UseMode: Credential}
 }
